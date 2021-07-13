b3b268f6b73e4a394fba31833b80f79c18324c10
Francesco-Grilli <66123653+Francesco-Grilli@users.noreply.github.com>
2021-07-13 14:52:56 +0200

    Debugging

--- API2021.c
+++ API2021.c
@@ -119,6 +119,52 @@ void addGraph() {
 
 }
 
+    for(i=1; i<numberNode; i++){
+
+        for(j=0; j<numberNode; j++){
+
+            if(scanf("%lu,", &value));
+            if(value>0 && i!=j)
+                addElemToList(list[i], value, j);
+            if(value!=start)
+                allEquals=false;
+
+        }
+
+        list[i]->distance=INF;
+    }
+
+    if(allEquals){
+        clearAll();
+        return value*(numberNode-1);
+    }
+
+    ubig sum=0;
+
+    do{
+        headList u = getMinHeapElement(heap);
+
+        if(u->distance<INF)
+            sum+=u->distance;
+
+        for(i=0; i<u->currentSize; i++){
+            if(!list[u->elemArr[i]]->wasInHeap){
+                list[u->elemArr[i]]->distance=u->distance + u->distanceArr[i];
+                addMinHeapElement(heap, list[u->elemArr[i]]);
+            }
+            else if(list[u->elemArr[i]]->inHeap==1){
+                ubig alt = u->distance + u->distanceArr[i];
+                if(list[u->elemArr[i]]->distance > alt){
+                    list[u->elemArr[i]]->distance=alt;
+                    minHeapFixUp(heap, list[u->elemArr[i]]->heapPosition);
+                }
+            }
+
+        }
+    }while(heap->currentSize>0);
+
+    clearAll();
+    return sum;
 
 headList createList(){
 
